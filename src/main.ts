3f682bb98c53b5db4759d7e794a4f2efa03303d5
Alexander Nikiforov <alexandernikser@gmail.com>
2019-04-25 22:38:09 +0500

    add fb auth btn

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import FBSignInButton from 'vue-facebook-signin-button'
+Vue.use(FBSignInButton)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <fb-signin-button
+      :params="fbSignInParams"
+      @success="onSignInSuccess"
+      @error="onSignInError">
+      Sign in with Facebook
+  </fb-signin-button>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from 'vue'
+import axios from 'axios'
 
 export default Vue.extend({
   name: 'home',
   components: {
-    HelloWorld,
+  
   },
+  data () {
+    return {
+      fbSignInParams: {
+        scope: 'email',
+        return_scopes: true
+      }
+    }
+  },
+  methods: {
+    onSignInSuccess (response: Response) {
+      FB.api('/me', dude => {
+        console.log(`Good to see you, ${dude.name}.`)
+        console.log(response)
+        axios.post('https://188.166.96.198:1338/login/facebook', {accessToken: response.authResponse.accessToken}).then(res => {
+          console.log(res)
+        })
+      })
+    },
+    onSignInError (error: Error) {
+      console.log('OH NOES', error)
+    }
+  },
+  mounted() {
+    axios.get('https://188.166.96.198:1338/users/leaderboard').then(res => {
+      console.log(res.data)
+    })
+  }
 });
 </script>
+<style>
+.fb-signin-button {
+  cursor: pointer;
+  display: inline-block;
+  padding: 4px 8px;
+  border-radius: 3px;
+  background-color: #4267b2;
+  color: #fff;
+}
+</style>
