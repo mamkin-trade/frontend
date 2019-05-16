cfacf89cbbcce5b056ea2f34e9c654442dbe29a1
Kakotkin Alexei <a.kakotkin@rambler-co.ru>
2019-05-16 17:33:29 +0300

    Chart

---
+++ src/components/Chart.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  div
+    v-card(flat)
+      v-card-title {{$t("chart.title")}}
+      TradingView(:options="tvOptions")
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import * as store from "../plugins/store";
+import TradingView from "../components/TradingView.vue";
+
+@Component({
+  components: {
+    TradingView
+  }
+})
+export default class Home extends Vue {
+  get tvOptions() {
+    return {
+      symbol: "BITFINEX:" + store.pair(),
+      theme: store.dark() ? "dark" : "light",
+      locale: store.language() || "en",
+      autosize: false,
+      width: 'auto',
+      height: 500,
+    };
+  }
+}
+</script>
