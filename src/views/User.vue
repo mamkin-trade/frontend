5964dddf36f34554d2b016fa2ec978cce4d1c840
backmeupplz <backmeupplz@gmail.com>
2019-05-19 23:28:13 -0700

    simplify user

--- src/views/User.vue
+++ src/views/User.vue
@@ -3,24 +3,18 @@
     v-btn(flat small @click='subscribe') {{$t('subscribe')}}
     v-layout(row wrap)
       v-flex.pa-1(xs12 md4 lg3)
-        Balance(:userId='id')
+        Balance(:userId='$route.params.id')
       v-flex.pa-1(xs12 md8 lg12)
-        Orders(:userId='id')
+        Orders(:userId='$route.params.id')
     .text-xs-center.pt-4
       div(v-html='$t("support")')
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import axios from "axios";
-import Leaderboard from "../components/Leaderboard.vue";
-import Tickers from "../components/Tickers.vue";
 import Orders from "../components/Orders.vue";
 import Balance from "../components/Balance.vue";
-import OrderForm from "../components/OrderForm.vue";
-import * as store from "../plugins/store";
 import Component from "vue-class-component";
-import { formatNumber } from "../utils/format";
 
 @Component({
   components: {
@@ -29,14 +23,11 @@ import { formatNumber } from "../utils/format";
   }
 })
 export default class Cabinet extends Vue {
-  formatNumber = formatNumber;
-
-  get id() {
-    return this.$route.params.id;
-  }
-
   subscribe() {
-    window.open(`https://t.me/mamkintrade_bot?start=${this.id}`, "_blank");
+    window.open(
+      `https://t.me/mamkintrade_bot?start=${this.$route.params.id}`,
+      "_blank"
+    );
   }
 }
 </script>
